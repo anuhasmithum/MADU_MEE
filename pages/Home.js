b730b60a3a84ec42162457850edc067f25c5c2e0
Mithum Anuhas <mithum@liveroom.xyz>
2020-10-01 10:02:44 +0530

    first commit

--- pages/Home.js
+++ pages/Home.js
@@ -1,18 +1,21 @@
 
+import Layout from './Layout'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router'
 
 
 export default function Main() {
-    const router = useRouter()
+    // const router = useRouter()
     return (
         <dev>
             <Head>
                 <title>...</title>
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></link>
             </Head>
-            <h3>Welcome to getstart...</h3>
-            <a onClick={() => router.push('/Home')}> login..</a>
+            <Layout>
+                <h3>Welcome to getstart...</h3>
+            </Layout>
+            <button type="button" className="btn btn-primary">click</button>
 
         </dev>
 
